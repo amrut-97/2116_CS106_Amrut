b98e087b1e74790418b06e2063058bc276a4a8ac
Amrut Adappa <30979419+amrut-97@users.noreply.github.com>
2021-09-19 19:40:05 +0530

    Update Client.java

--- Exercise_04/src/Client.java
+++ Exercise_04/src/Client.java
@@ -4,7 +4,7 @@ public class Client {
 	public static void main(String[] args) {   
 	
 		LinkedList list = new LinkedList();
-		//list.updateAt(0, 0); // Displays error message when updating when list is empty
+		//list.updateAt(0, 0); // Displays error message about updating when list is empty
 		//list.show();  // Displays error message list when its empty
 		
 		list.insert(11);
@@ -14,11 +14,11 @@ public class Client {
 		
 		//list.show(); // shows 11 34 25 54
 		
-		list.updateAt(0, 0);
+		list.updateAt(0, 9);
 		list.updateAt(1, 3);
 		list.updateAt(3, 0);
 		
-		//list.show();  // shows  0 3 25 0
+		//list.show();  // shows  9 3 25 0
 		
 		list.deleteAt(3);
 		list.deleteAt(2);
