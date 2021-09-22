4cdc6fb49553888a3b20bfe8fc5f2a3e0b867b72
Amrut Adappa <30979419+amrut-97@users.noreply.github.com>
2021-09-22 20:31:43 +0530

    Add files via upload

---
+++ Exercise_06/src/MultitaskingStaff.java
@@ -0,0 +1,12 @@
+
+public class MultitaskingStaff extends Staff{
+
+	public MultitaskingStaff(){
+		
+	}
+	
+	public MultitaskingStaff(String name, Integer salary) {
+		super(name, salary);
+	}
+
+}
